69060776c5dadeafc46fcfff5c32d4e5fd9fb14c
gafservice <39964081+gafservice@users.noreply.github.com>
2025-07-18 15:10:46 -0600

    Update formulario_alquiler.py

--- formulario_alquiler.py
+++ formulario_alquiler.py
@@ -17,6 +17,10 @@ st.success("Gracias por su interés en alquilar una de nuestras propiedades. Est
 
 
 st.image("fachada1.jpg", caption="Frente al Palí, Higuito Centro", use_container_width=True)
+st.image("fCarac_Servi.png", caption="Frente al Palí, Higuito Centro", use_container_width=True)
+
+
+
 st.video("https://youtu.be/9U7l9rvnVJc")
 st.markdown("### ⚠️ Nota de Confidencialidad y Verificación de Información")
 st.info(

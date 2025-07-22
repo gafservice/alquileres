136e780ae700ba72b603a43f25fa3dda53521f4e
gafservice <39964081+gafservice@users.noreply.github.com>
2025-07-22 11:04:51 -0600

    Update formulario_alquiler.py

--- formulario_alquiler.py
+++ formulario_alquiler.py
@@ -68,8 +68,9 @@ form_data["Pago servicios"] = st.radio("¿Quién se encargará del pago de los s
 form_data["Monto alquiler estimado"] = st.text_input("¿Cuánto estaría dispuesto a pagar por el alquiler mensual?")
 form_data["Observaciones"] = st.text_area("Observaciones adicionales")
 form_data["Consentimiento"] = st.checkbox("Declaro que la información proporcionada es verdadera y autorizo su verificación.", value=False)
-form_data["Consentimiento datos"] = st.checkbox("Autorizo su eliminación si no se formaliza un contrato.", value=False)
 archivo = st.file_uploader("Opcional: Adjunte foto, referencia o documento", type=["png", "jpg", "jpeg", "pdf"])
+form_data["Consentimiento datos"] = st.checkbox("Autorizo su eliminación si no se formaliza un contrato.", value=False)
+
 
 if st.button("Enviar solicitud"):
     if not form_data["Consentimiento"] or not form_data["Consentimiento datos"]:
@@ -132,13 +133,6 @@ if st.button("Enviar solicitud"):
             if enviar_confirmacion:
                 cuerpo_usuario = f"""Estimado/a {form_data.get("Nombre completo", "interesado/a")},
 
-st.markdown("### ⚠️ Nota de Confidencialidad y Verificación de Información")
-st.info(
-    "La información que usted proporcione será tratada con estricta confidencialidad y utilizada únicamente para fines de evaluación de su solicitud de alquiler. "
-    "Todos los datos personales, referencias y documentos adjuntos podrán ser verificados. "
-    "Ningun dato será compartirido ni almacenado sin su autorización explicita, si no se formaliza el contrato, los datos serán eliminados en su todalidad.\n\n"
-    "Al continuar, usted acepta estos términos."
-)
 
 
 
@@ -179,4 +173,11 @@ Administración de Propiedades
 
         # ✅ Confirmación final
         st.success("✅ ¡Solicitud enviada con éxito!")
+        st.markdown("### ⚠️ Nota de Confidencialidad y Verificación de Información")
+        st.info("La información que usted proporcione será tratada con estricta confidencialidad y utilizada únicamente para fines de evaluación de su solicitud de alquiler. "
+            "Todos los datos personales, referencias y documentos adjuntos podrán ser verificados. "
+            "Ningun dato será compartirido ni almacenado sin su autorización explicita, si no se formaliza el contrato, los datos serán eliminados en su todalidad.\n\n"
+            "Al continuar, usted acepta estos términos.")
+
+        
         st.info("Si desea generar un sistema similar para el alquiler de sus bienes inmuebles, puede contactarnos a: info@vigias.net") 
